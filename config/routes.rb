943431354e44218bd4267d90d2280803ba8b60a9
hubodashi <hubodashi@aliyun.com>
2017-05-24 20:10:32 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
   devise_for :users
-
+  resource :user
   resources :events
 
   namespace :admin do
     root "events#index"
-    resources :events
+   resources :events do
+     resources :tickets, :controller => "event_tickets"
+   end
+    resources :users do
+      resource :profile, :controller => "user_profiles"
+    end
   end
 
   root "events#index"
